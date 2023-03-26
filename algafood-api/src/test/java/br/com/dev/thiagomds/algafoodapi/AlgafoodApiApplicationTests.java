84b50585e4b859595f13f0ece1d97b5c68c87386
Thiago Mendes de Sousa <thiagomendes_95@hotmail.com>
2023-03-26 19:37:44 -0300

    Commit Inicial

---
+++ algafood-api/src/test/java/br/com/dev/thiagomds/algafoodapi/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.thiagomds.algafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
